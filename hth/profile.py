c34063a28d1ac382cb5753c6183f6ef1e2df2d74
Lam Ngu <lam@Lams-MacBook-Pro-M5.local>
2026-06-16 17:05:25 -0700

    Add Python profile variables lab

---
+++ hth/profile.py
@@ -0,0 +1,19 @@
+full_name = "Lam Ngu"
+nick_name = "Lam"
+age = 16
+#print(full_name, nick_name)
+used_python_before = False
+
+things_ilike = {"foods": "steak", 
+    "colors": "gray", 
+    "favourite animal": "dog"}
+hobbies = ["volleyball", "basketball", "reading", "video games"]
+ #print(full_name, age, things_ilike, hobbies)
+ #extra
+skills = ("communication", "teamwork")
+languages_spoken = {"English", "Vietnamese"}
+everything = (
+    full_name, age, 
+    things_ilike, skills, 
+    languages_spoken)
+print(everything)
